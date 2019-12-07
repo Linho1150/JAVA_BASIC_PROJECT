80b0a7867e9d90819bf9ef96da0911fc7eef5334
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2019-12-08 03:15:21 +0900

    total_price, amount, new Window

--- src/MAIN/MAIN.java
+++ src/MAIN/MAIN.java
@@ -4,44 +4,96 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.List;
 
-import javax.swing.JButton;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableModel;
 import javax.swing.*;
 
+class addFrame extends JFrame {
+	private JButton ok = new JButton("확인");
+	private JTextField tx = new JTextField(20);
+	private JTable table;
+	private JTable table2;
+	private JLabel total_price;
+
+	public addFrame(JTable table, JTable table2, JLabel total_price) {
+		this.table = table;
+		this.table2 = table2;
+		this.total_price = total_price;
+
+		setTitle("add");
+		Font f1 = new Font("돋움", Font.PLAIN, 10);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+
+		JLabel lb = new JLabel("수량 입력");
+
+		lb.setFont(f1);
+		ok.setFont(f1);
+
+		c.add(lb);
+		c.add(tx);
+		c.add(ok);
+
+		setSize(300, 150);
+		setVisible(true);
+		ok.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setFrame();
+			}
+		});
+	}
+
+	public void setFrame() {
+		int tmp = Integer.parseInt(total_price.getText());
+		TableModel copymodel = table.getModel();
+		int[] indexs = table.getSelectedRows();
+		DefaultTableModel copymode2 = (DefaultTableModel) table2.getModel();
+		Object[] copyrow = new Object[8];
+		for (int i = 0; i < indexs.length; i++) {
+			copyrow[0] = copymodel.getValueAt(indexs[i], 0);
+			copyrow[1] = copymodel.getValueAt(indexs[i], 1);
+			copyrow[2] = copymodel.getValueAt(indexs[i], 2);
+			copyrow[3] = copymodel.getValueAt(indexs[i], 3);
+			copyrow[4] = copymodel.getValueAt(indexs[i], 4);
+			copyrow[5] = tx.getText();
+			copymode2.addRow(copyrow);
+			tmp = tmp + (Integer.parseInt(tx.getText()) * Integer.parseInt(copyrow[3].toString()));
+			System.out.print(tmp);
+		}
+		total_price.setText(String.valueOf(tmp));
+	}
+}
 
 public class MAIN extends JFrame {
-	public String header[] = {"품목명","단위","등급","가격","산지","친환경구분","입력일"};
+	public String header[] = { "품목명", "단위", "등급", "가격", "산지", "친환경구분", "입력일" };
+	public String header2[] = { "품목명", "단위", "등급", "가격", "산지", "수량" };
 	public Object combo1 = "";
 	public Object combo2 = "";
 
 	MAIN() {
-		//초기세팅---------------------------------------------------
+		// 초기세팅---------------------------------------------------
 		setTitle("농수산물 구매 프로세스");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		Container c = getContentPane();
-		////----------------------------------------------------------
-		
-		//첫화면 실행---------------------------------------------------
-		Tools t1=new Tools();
+		//// ----------------------------------------------------------
+
+		// 첫화면 실행---------------------------------------------------
+		Tools t1 = new Tools();
 		List<List<String>> conten = t1.run();
-		////----------------------------------------------------------
-		
-		//Jtabel에 넣기 위한 List to String array ---------------------------------------------------
+		//// ----------------------------------------------------------
+
+		// Jtabel에 넣기 위한 List to String array
+		// ---------------------------------------------------
 		String[][] array = new String[conten.size()][];
-		/*for (int i = 0; i < conten.size(); i++) {
-		    List<String> row = conten.get(i);
-		    array[i] = row.toArray(new String[row.size()]);
-		}*/
-		////----------------------------------------------------------
-		
+		/*
+		 * for (int i = 0; i < conten.size(); i++) { List<String> row = conten.get(i);
+		 * array[i] = row.toArray(new String[row.size()]); }
+		 */
+		//// ----------------------------------------------------------
+
 		JPanel j1 = new JPanel();
 		JPanel j2 = new JPanel();
 
@@ -54,19 +106,21 @@ public class MAIN extends JFrame {
 		// panel j1
 		// Jcombobox1
 		JLabel name = new JLabel("category");
-		String[] namelist=t1.getItemlist();
+		String[] namelist = t1.getItemlist();
 		JComboBox<String> comboname = new JComboBox<String>(namelist); // 생선이름데이터 리스트
 		j1.add(name);
 		j1.add(comboname);
-		
+
 		// Jcombobox2
 		JLabel name1 = new JLabel("cate123123gory");
-		String[] namelist1=t1.getItemlist();
+		String[] namelist1 = t1.getItemlist();
 		JComboBox<String> comboname1 = new JComboBox<String>(namelist1); // 생선이름데이터 리스트
 		j1.add(name1);
 		j1.add(comboname1);
 		comboname1.setEnabled(false);
 
+		JLabel total_price = new JLabel("0");
+		j1.add(total_price);
 
 		// panel j2
 		// JTable원산지
@@ -76,17 +130,17 @@ public class MAIN extends JFrame {
 		sc.setBounds(20, 20, 400, 200);
 		j2.add(sc);
 		sc.setEnabled(false);
-		
+
 		// 오른쪽 테이블
 		String contents2[][] = {};
-		DefaultTableModel model2 = new DefaultTableModel(contents2, header);
+		DefaultTableModel model2 = new DefaultTableModel(contents2, header2);
 		JTable table2 = new JTable(model2);
 		JScrollPane sc2 = new JScrollPane(table2);
 		sc2.setBounds(500, 20, 400, 200);
 		j2.add(sc2);
 		sc2.setEnabled(false);
 
-		//button
+		// button
 		Font f1 = new Font("돋움", Font.PLAIN, 10);
 		Font f2 = new Font("맑은고딕", Font.PLAIN, 10);
 		JButton add = new JButton("Add all");
@@ -184,68 +238,69 @@ public class MAIN extends JFrame {
 		comboname.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JComboBox jcb = (JComboBox) e.getSource();
-				combo1 = jcb.getSelectedItem();				
-				
+				combo1 = jcb.getSelectedItem();
+
 				comboname1.setEnabled(true);
 				comboname1.removeAllItems();
-				
-				//맞춤 table 가져오기---------------------------------------
+
+				// 맞춤 table 가져오기---------------------------------------
 				List<List<String>> tmp = new ArrayList<List<String>>();
-				for(int i=0; i<conten.size();i++) {
-				    List<String> row = conten.get(i);
-				    if(row.get(0).equals(combo1)) {
-				    	tmp.add(row);
-				    }
+				for (int i = 0; i < conten.size(); i++) {
+					List<String> row = conten.get(i);
+					if (row.get(0).equals(combo1)) {
+						tmp.add(row);
+					}
 				}
-				////---------------------------------------------------
-				
-				//TABLE에 맞는 형변환---------------------------------------
+				//// ---------------------------------------------------
+
+				// TABLE에 맞는 형변환---------------------------------------
 				String[][] array = new String[tmp.size()][];
 				for (int i = 0; i < tmp.size(); i++) {
-				    List<String> row = tmp.get(i);
-					array[i] = row.toArray(new String[row.size()]);					
-				}	
-				////---------------------------------------------------
-				
+					List<String> row = tmp.get(i);
+					array[i] = row.toArray(new String[row.size()]);
+				}
+				//// ---------------------------------------------------
+
 				DefaultTableModel model = new DefaultTableModel(array, header);
 				table.setModel(model);
 				List<String> numdata = new ArrayList<String>();
-			    for (int count = 0; count < model.getRowCount(); count++){
-			    	String tmp_numdata=model.getValueAt(count, 4).toString();
-			    	if(numdata.contains(tmp_numdata)) {continue;}else {
-			    		numdata.add(model.getValueAt(count, 4).toString());
-			    		comboname1.addItem(model.getValueAt(count, 4).toString());
-			    	}
-			    }
+				for (int count = 0; count < model.getRowCount(); count++) {
+					String tmp_numdata = model.getValueAt(count, 4).toString();
+					if (numdata.contains(tmp_numdata)) {
+						continue;
+					} else {
+						numdata.add(model.getValueAt(count, 4).toString());
+						comboname1.addItem(model.getValueAt(count, 4).toString());
+					}
+				}
 			}
 		});
-		
+
 		// combobox listener
 		comboname1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JComboBox jcb = (JComboBox) e.getSource();
 				combo2 = jcb.getSelectedItem();
-				
+
 				sc.setEnabled(true);
 				sc2.setEnabled(true);
 
 				List<List<String>> tmp = new ArrayList<List<String>>();
-				for(int i=0; i<conten.size();i++) {
-				    List<String> row = conten.get(i);
-				    if(row.get(0).equals(combo1)&& row.get(4).equals(combo2)) {
-				    	tmp.add(row);
-				    }
+				for (int i = 0; i < conten.size(); i++) {
+					List<String> row = conten.get(i);
+					if (row.get(0).equals(combo1) && row.get(4).equals(combo2)) {
+						tmp.add(row);
+					}
 				}
-				
+
 				String[][] array = new String[tmp.size()][];
 				for (int i = 0; i < tmp.size(); i++) {
-				    List<String> row = tmp.get(i);
-					array[i] = row.toArray(new String[row.size()]);					
-				}	
-				
+					List<String> row = tmp.get(i);
+					array[i] = row.toArray(new String[row.size()]);
+				}
+
 				DefaultTableModel model = new DefaultTableModel(array, header);
 				table.setModel(model);
-
 
 			}
 		});
@@ -253,22 +308,7 @@ public class MAIN extends JFrame {
 		// Add all button listener
 		add.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				TableModel copymodel = table.getModel();
-				int [] indexs = table.getSelectedRows();
-				Object[] copyrow = new Object[7];
-				DefaultTableModel copymode2=(DefaultTableModel) table2.getModel();
-				for(int i = 0; i<indexs.length; i++) {
-					copyrow[0]=copymodel.getValueAt(indexs[i], 0);
-					copyrow[1]=copymodel.getValueAt(indexs[i], 1);
-					copyrow[2]=copymodel.getValueAt(indexs[i], 2);
-					copyrow[3]=copymodel.getValueAt(indexs[i], 3);
-					copyrow[4]=copymodel.getValueAt(indexs[i], 4);
-					copyrow[5]=copymodel.getValueAt(indexs[i], 5);
-					copyrow[6]=copymodel.getValueAt(indexs[i], 6);
-
-					copymode2.addRow(copyrow);
-				}
-
+				addFrame addT = new addFrame(table, table2, total_price);
 			}
 		});
 
@@ -284,46 +324,42 @@ public class MAIN extends JFrame {
 
 class Tools {
 	List<List<String>> ret = new ArrayList<List<String>>();
-    List<String> itemList = new ArrayList<String>();
+	List<String> itemList = new ArrayList<String>();
 
 	public List<List<String>> run() {
 		BufferedReader br = null;
-	    String line;
-	    String cvsSplitBy = ";";
-        try {
-            br = new BufferedReader(new InputStreamReader(new FileInputStream("농수산물_경매_2018.csv"), "UTF-8"));
-            while ((line = br.readLine()) != null) {
-	            List<String> tmpList = new ArrayList<String>();
-	            line=line.replace("\"","");
-                String[] field = line.split(cvsSplitBy);
-                itemList.add(field[0]);              
-                tmpList = Arrays.asList(field);
-	            ret.add(tmpList);
-            }
-            System.out.print("안내 : CSV파일을 변수로 저장하였습니다.\n");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+		String line;
+		String cvsSplitBy = ";";
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream("농수산물_경매_2018.csv"), "UTF-8"));
+			while ((line = br.readLine()) != null) {
+				List<String> tmpList = new ArrayList<String>();
+				line = line.replace("\"", "");
+				String[] field = line.split(cvsSplitBy);
+				itemList.add(field[0]);
+				tmpList = Arrays.asList(field);
+				ret.add(tmpList);
+			}
+			System.out.print("안내 : CSV파일을 변수로 저장하였습니다.\n");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		return ret;
-    }
+	}
 
 	public String[] getItemlist() {
-	    HashSet<String> tmp_hasharray = new HashSet<String>(itemList);
-	    ArrayList<String> tmp_itemList = new ArrayList<String>(tmp_hasharray);
-	    System.out.print("안내 : itemList 로딩을 완료하였습니다.\n");
-	    String[] stockArr = new String[tmp_itemList.size()];
-	    stockArr = tmp_itemList.toArray(stockArr);
-	    return stockArr;
+		HashSet<String> tmp_hasharray = new HashSet<String>(itemList);
+		ArrayList<String> tmp_itemList = new ArrayList<String>(tmp_hasharray);
+		System.out.print("안내 : itemList 로딩을 완료하였습니다.\n");
+		String[] stockArr = new String[tmp_itemList.size()];
+		stockArr = tmp_itemList.toArray(stockArr);
+		return stockArr;
 	}
 
 }
